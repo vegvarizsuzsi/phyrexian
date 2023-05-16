20a1a70625b76f686149705b2f68321dc118de13
Zsuzsi Vegvari <zsuzsanna.vegvari@gmail.com>
2023-05-16 09:58:53 +0200

    restricted the movement

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.Drawable;
 
 public abstract class Actor implements Drawable {
@@ -14,6 +15,15 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
+        if (nextCell.getType() == CellType.WALL) {
+            // The next cell is a wall, the player cannot move there
+            return;
+        }
+
+        if (nextCell.getActor() != null) {
+            // The next cell is an actor, the player cannot move there
+            return;
+        }
         cell.setActor(null);
         nextCell.setActor(this);
         cell = nextCell;
