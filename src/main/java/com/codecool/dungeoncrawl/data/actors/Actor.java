f92ec1170f6267d139d5846777a562ecc5184e58
Zsuzsi Vegvari <zsuzsanna.vegvari@gmail.com>
2023-05-16 14:59:58 +0200

    disappear the items when player standing on it

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.data.actors;
 import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.Drawable;
+import com.codecool.dungeoncrawl.data.items.Item;
 
 public abstract class Actor implements Drawable {
     private Cell cell;
@@ -24,9 +25,20 @@ public abstract class Actor implements Drawable {
             // The next cell is an actor, the player cannot move there
             return;
         }
+        if (nextCell.getItem() != null) {
+            Item item = nextCell.getItem();
+            pickUpItem(item);
+            nextCell.setItem(null);
+        }
+
         cell.setActor(null);
         nextCell.setActor(this);
         cell = nextCell;
+    }
+
+    public void pickUpItem(Item item) {
+        // Implement the logic to handle picking up the item
+        // For example, you can modify the player's state or inventory based on the item
     }
 
     public int getHealth() {
