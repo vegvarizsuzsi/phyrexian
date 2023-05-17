8fde68634565d3f3626dc4d08d3d118f4bd27398
Zsuzsi Vegvari <zsuzsanna.vegvari@gmail.com>
2023-05-17 11:08:43 +0200

    refactor the movement

---
+++ src/main/java/com/codecool/dungeoncrawl/data/Movement.java
@@ -0,0 +1,5 @@
+package com.codecool.dungeoncrawl.data;
+
+public interface Movement {
+    public void move(int dx, int dy);
+}
--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -9,50 +9,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Actor implements Drawable {
-    private Cell cell;
-    private int health = 10;
-    private List<Item> inventory;
+    protected Cell cell;
+//    private int health = 10;
+//    private List<Item> inventory;
 
     public Actor(Cell cell) {
         this.cell = cell;
         this.cell.setActor(this);
-        this.inventory = new ArrayList<>();
+//        this.inventory = new ArrayList<>();
     }
 
-    public void move(int dx, int dy) {
-        Cell nextCell = cell.getNeighbor(dx, dy);
-        if (nextCell.getType() == CellType.WALL) {
-            // The next cell is a wall, the player cannot move there
-            return;
-        }
+//    public void move(int dx, int dy) {
+//        Cell nextCell = cell.getNeighbor(dx, dy);
+//        if (nextCell.getType() == CellType.WALL) {
+//            // The next cell is a wall, the player cannot move there
+//            return;
+//        }
+//
+//        if (nextCell.getActor() != null) {
+//            // The next cell is an actor, the player cannot move there
+//            return;
+//        }
+//        if (nextCell.getItem() != null) {
+//            Item item = nextCell.getItem();
+//            pickUpItem(item);
+//            nextCell.setItem(null);
+//        }
+//
+//        cell.setActor(null);
+//        nextCell.setActor(this);
+//        cell = nextCell;
+//    }
 
-        if (nextCell.getActor() != null) {
-            // The next cell is an actor, the player cannot move there
-            return;
-        }
-        if (nextCell.getItem() != null) {
-            Item item = nextCell.getItem();
-            pickUpItem(item);
-            nextCell.setItem(null);
-        }
+//    public void pickUpItem(Item item) {
+//        inventory.add(item);
+//        health += 10;
+//    }
+//
+//    public List<Item> getInventory() {
+//        return inventory;
+//    }
 
-        cell.setActor(null);
-        nextCell.setActor(this);
-        cell = nextCell;
-    }
-
-    public void pickUpItem(Item item) {
-        inventory.add(item);
-        health += 10;
-    }
-
-    public List<Item> getInventory() {
-        return inventory;
-    }
-
-    public int getHealth() {
-        return health;
-    }
+//    public int getHealth() {
+//        return health;
+//    }
 
     public Cell getCell() {
         return cell;
